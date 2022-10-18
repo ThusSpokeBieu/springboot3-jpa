8b4d340215328759d2a1fb4ecf1427617ba47db0
Gabriel Messias Costa <gmessiasc@gmail.com>
2022-10-18 18:25:37 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/gmessiasc/SpringWebService/config/TestConfig.java
+++ src/main/java/com/gmessiasc/SpringWebService/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.gmessiasc.SpringWebService.config;
 
-import com.gmessiasc.SpringWebService.entities.Category;
-import com.gmessiasc.SpringWebService.entities.Order;
-import com.gmessiasc.SpringWebService.entities.Product;
-import com.gmessiasc.SpringWebService.entities.User;
+import com.gmessiasc.SpringWebService.entities.*;
 import com.gmessiasc.SpringWebService.entities.enums.OrderStatus;
-import com.gmessiasc.SpringWebService.repositories.CategoryRepository;
-import com.gmessiasc.SpringWebService.repositories.OrderRepository;
-import com.gmessiasc.SpringWebService.repositories.ProductRepository;
-import com.gmessiasc.SpringWebService.repositories.UserRepository;
+import com.gmessiasc.SpringWebService.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -69,5 +66,12 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
--- src/main/java/com/gmessiasc/SpringWebService/entities/Order.java
+++ src/main/java/com/gmessiasc/SpringWebService/entities/Order.java
@@ -7,7 +7,9 @@ import jakarta.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -27,6 +29,9 @@ public class Order implements Serializable {
     private User client;
 
     private Integer orderStatus;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){
 
@@ -72,6 +77,10 @@ public class Order implements Serializable {
         if(orderStatus != null){
             this.orderStatus = orderStatus.getCode();
         }
+    }
+
+    public Set<OrderItem> getItems(){
+        return items;
     }
 
     @Override
