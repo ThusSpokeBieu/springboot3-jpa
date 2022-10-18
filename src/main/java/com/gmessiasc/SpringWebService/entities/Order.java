be31015e582c6e24645359045b376a8e3d822889
Gabriel Messias Costa <gmessiasc@gmail.com>
2022-10-17 23:14:03 -0300

    OrderStatus enum

--- src/main/java/com/gmessiasc/SpringWebService/entities/Order.java
+++ src/main/java/com/gmessiasc/SpringWebService/entities/Order.java
@@ -1,7 +1,7 @@
 package com.gmessiasc.SpringWebService.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.gmessiasc.SpringWebService.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -26,13 +26,17 @@ public class Order implements Serializable {
     @JoinColumn(name = "clientId")
     private User client;
 
+    private Integer orderStatus;
+
     public Order(){
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -60,6 +64,16 @@ public class Order implements Serializable {
         this.client = client;
     }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -79,6 +93,7 @@ public class Order implements Serializable {
                 "id=" + id +
                 ", moment=" + moment +
                 ", client=" + client +
+                ", orderStatus=" + orderStatus +
                 '}';
     }
 }
